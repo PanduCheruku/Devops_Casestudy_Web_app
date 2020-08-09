e56290e06724f2bfca136cf7cb2d43ecf2538bf2
Rahul Kalithkar <rahulkalithkar1996@gmail.com>
2020-08-09 15:08:56 +0530

    modified installation.yml file

--- installations.sh
+++ installations.sh
@@ -3,4 +3,4 @@ sudo apt-get update -y
 sudo apt-get install docker.io -y
 sudo usermod -aG docker ubuntu
 sudo service docker start
-sudo docker run -p 8585:8080 -d kalithkar/casestudy_webapp:2.0
+sudo docker run -p 8585:8080 -d kalithkar/casestudy_webapp:3.0
